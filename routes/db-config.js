5d99069465481422a0d603812a7f6deb09e0b2da
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-03-25 12:41:36 +0530

    db suer 4

--- routes/db-config.js
+++ routes/db-config.js
@@ -2,12 +2,12 @@
 const dotenv = require("dotenv").config();
 
 const sql = require("mysql");
-// const db = sql.createConnection({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_NAME
-// })
-const db = sql.createConnection(`mysql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`);
+const db = sql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+})
+//const db = sql.createConnection(`mysql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`);
 
 module.exports = db;
